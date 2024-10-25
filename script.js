859374ebf202ce4fe60219c2473530a2aa26c0e5
KoZsombat <125221225+KoZsombat@users.noreply.github.com>
2024-10-25 18:33:33 +0200

    website

---
+++ script.js
@@ -0,0 +1,12 @@
+function Szamol(){
+    let k = parseInt(document.getElementById("kill").value);
+    let d = parseInt(document.getElementById("death").value);
+    let a = parseInt(document.getElementById("assist").value);
+    let tf = parseInt(document.getElementById("topfrag").value);
+    let f = parseInt(document.getElementById("frag").value);
+    let ossz = a+k-d+(k-tf+5)-(f-1)
+    if (ossz > 0)
+    document.getElementById("kimenet").innerText = "Jó voltál"
+    else
+    document.getElementById("kimenet").innerText = "Van hova fejlődni"   
+}
