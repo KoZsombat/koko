50d637ceab44ec7d994dfbf8c44e2db0b478e6be
KoZsombat <125221225+KoZsombat@users.noreply.github.com>
2024-10-25 18:49:20 +0200

    fix

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function Szamol(){
     let k = parseInt(document.getElementById("kill").value);
-    if (k < 10){
-        document.getElementById("kimenet").innerText = "Jack Fitness"
-        return
-    }
     let d = parseInt(document.getElementById("death").value);
     let a = parseInt(document.getElementById("assist").value);
     let tf = parseInt(document.getElementById("topfrag").value);
     let f = parseInt(document.getElementById("frag").value);
     let ossz = a+k-d+(k-tf+5)-(f-1)
-    if (ossz > 0)
-    document.getElementById("kimenet").innerText = "Jó voltál"
-    else
-    document.getElementById("kimenet").innerText = "Van hova fejlődni"   
+    if (k < 10){
+        document.getElementById("kimenet").innerText = "Jack Fitness"
+    }
+    else{
+        if (ossz > 0)
+            document.getElementById("kimenet").innerText = "Jó voltál"
+        else
+            document.getElementById("kimenet").innerText = "Van hova fejlődni"   
+    }
 }
