a97006487de2578720f2d32fbd5010ea5b6d5668
KoZsombat <125221225+KoZsombat@users.noreply.github.com>
2024-10-25 18:50:59 +0200

    FIX

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function Szamol(){
     let tf = parseInt(document.getElementById("topfrag").value);
     let f = parseInt(document.getElementById("frag").value);
     let ossz = a+k-d+(k-tf+5)-(f-1)
-    if (k < 10){
+    if (k < 10)
         document.getElementById("kimenet").innerText = "Jack Fitness"
-    }
-    else{
+    else
+    {
         if (ossz > 0)
             document.getElementById("kimenet").innerText = "Jó voltál"
         else
